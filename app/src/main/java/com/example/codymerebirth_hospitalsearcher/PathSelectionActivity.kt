421b32a6331e0a61abad773d4b0a2fc37ad51408
Marcos Simon <marcosht503@gmail.com>
2021-09-05 20:58:58 -0400

    Diseño de pantalla de selección de puntos de inicio y final. Añadido Floyd Warshall.

--- app/src/main/java/com/example/codymerebirth_hospitalsearcher/PathSelectionActivity.kt
+++ app/src/main/java/com/example/codymerebirth_hospitalsearcher/PathSelectionActivity.kt
@@ -7,5 +7,7 @@ class PathSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_path_selection)
+
+
     }
 }
